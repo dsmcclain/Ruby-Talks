0111831dd2253e6511732f93ef625336dc3b53ff
Dylan McClain <dylan.mcclain@gmail.com>
2019-03-24 14:59:16 -0700

    change arguments for sentence-filtering method

--- wordplay.rb
+++ wordplay.rb
@@ -1,7 +1,8 @@
 class WordPlay
-  def self.most_important_sentence(sentences, key_words)
-    ranked_sentences = sentences.sort_by do |s|
-      s.words.length - (s.downcase.words - key_words).length
+  def self.most_important_sentence(text, key_words)
+    lines = text.sentences
+    ranked_sentences = lines.sort_by do |line|
+      line.words.length - (line.downcase.words - key_words).length
     end
 
     ranked_sentences.last
@@ -22,4 +23,4 @@ end
 
 my_string = "Brexit is a total disaster. They can't seem to agree on anything.  If the UK leaves the EU, it will be a failure."
 key_words = %w{brexit, uk, eu}
-puts WordPlay.most_important_sentence(my_string.sentences, key_words)
+puts WordPlay.most_important_sentence(my_string, key_words)
