cfdefc3f845f7af8ba61becda457b56b44e3ae8c
Dylan McClain <dylan.mcclain@gmail.com>
2019-03-24 15:09:35 -0700

    add pronoun-switching method

--- wordplay.rb
+++ wordplay.rb
@@ -7,6 +7,25 @@ class WordPlay
 
     ranked_sentences.last
   end
+
+  def self.switch_pronouns(text)
+    text.gsub(/\b(I am|You are|I|You|Your|My)\b/i) do |pronoun|
+      case pronoun.downcase
+        when "i"
+          "you"
+        when "you"
+          "me"
+        when "i am"
+          "you are"
+        when "you are"
+          "i am"
+        when "your"
+          "my"
+        when "my"
+          "your"
+      end
+    end.sub(/^me\b/i, 'i')
+  end
 end
 
 #splits on a period followed by whitespace. 
@@ -24,3 +43,6 @@ end
 my_string = "Brexit is a total disaster. They can't seem to agree on anything.  If the UK leaves the EU, it will be a failure."
 key_words = %w{brexit, uk, eu}
 puts WordPlay.most_important_sentence(my_string, key_words)
+
+puts WordPlay.switch_pronouns("Your cat is not a creature I like.")
+puts WordPlay.switch_pronouns("You think it is time for me to go")
