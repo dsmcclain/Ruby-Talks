25cb951ec58b17e3bbb30be73c3ef9cee3ee6c6d
Dylan McClain <dylan.mcclain@gmail.com>
2019-03-24 17:04:04 -0700

    create basic data file and write to yaml format

---
+++ RT_data_to_yaml.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+
+ruby_talks_data = {
+  :presubs => [
+    ["dont", "don't"],
+    ["youre", "you're"],
+    ["ur", "your"]
+  ],
+  :responses => {
+    :default        => [
+                        "I don't understand.",
+                        "What?",
+                        "Huh?"
+                      ],
+    :greeting       => ["Hi there. I'm Ruby Talks. What's happening?"],
+    :farewell       => ["Goodbye!"],
+    'hello'         => [
+                        "How's it going?",
+                        "Hi there!"
+                       ],
+  }
+}
+
+puts ruby_talks_data.to_yaml
+
+f = File.open(ARGV.first || 'RT_data', "w")
+f.puts ruby_talks_data.to_yaml
+f.close
